5e6ff916a0e29a7e79818d8d06d1d62b0ed2961f
wierje <wier.je@gmail.com>
2016-12-01 14:57:10 -0600

    basic loop and output

--- stackLetters.js
+++ stackLetters.js
@@ -1,6 +1,6 @@
-var stack = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-stack.sort();
-console.log(stack);
-for (var previous = stack[0]; previous < stack.length(); stack++) {
-    console.log(previous);
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+alphabet.sort();
+console.log(alphabet);
+for (var i = 0; i < alphabet.length; i++) {
+    console.log(alphabet[i]);
 }
