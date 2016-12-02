ccb3cf3e693e740f46078cc09c28930f11a189aa
wierje <wier.je@gmail.com>
2016-12-02 09:46:08 -0600

    stackletters working

--- stackLetters.js
+++ stackLetters.js
@@ -1,9 +1,11 @@
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 alphabet.sort();
-console.log(alphabet);
+// create a container to hold the stacked letters
+var stack = [];
+// console.log(alphabet);
+// loop through the alphabet
 for (var i = 0; i < alphabet.length; i++) {
-    console.log(alphabet[i] + (" ".repeat(alphabet.length -i )))
+    // load the stack array
+    stack.push(alphabet[i]);
+    console.log(stack + (" ".repeat(alphabet.length - i)))
 }
-
-
-// console.log(' '.repeat(treeHeight-i) + treeCharacter.repeat(counter))
