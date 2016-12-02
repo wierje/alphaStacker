451899e482f414a6960d711d9aff2cde0f2a2f64
wierje <wier.je@gmail.com>
2016-12-02 13:28:24 -0600

    exercise finished

--- stackLetters.js
+++ stackLetters.js
@@ -1,11 +1,17 @@
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-alphabet.sort();
+
+function stackLetters (theAlphabetArray) {
 // create a container to hold the stacked letters
 var stack = [];
-// console.log(alphabet);
 // loop through the alphabet
 for (var i = 0; i < alphabet.length; i++) {
     // load the stack array
     stack.push(alphabet[i]);
-    console.log(stack + (" ".repeat(alphabet.length - i)))
+    //insert spaces
+    if ((i + 1) % 3 === 0) {
+        stack.push(" ");
+    }
+    console.log(stack.join("")); //+ (" ".repeat(alphabet.length - i)));
+  }
 }
+stackLetters(alphabet);
